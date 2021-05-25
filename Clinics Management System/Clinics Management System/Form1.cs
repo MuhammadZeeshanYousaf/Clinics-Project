35c654de85848ce9d9b2bd7aeae8828d6d05ad4c
ahmedfaisal-0123 <ahmedfaisal0123@gmail.com>
2021-05-26 02:02:54 +0500

    ReceptionistBuild

---
+++ Clinics Management System/Clinics Management System/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clinics_Management_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Dashboard_tab_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegPat_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PatCNIC_lbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
