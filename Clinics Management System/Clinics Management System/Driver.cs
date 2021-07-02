07d7cf834c425dd469c93e32644ae221999a6c28
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2021-07-02 09:44:03 +0500

    (Clinics Management System/Clinics Management System/ClinicsDatabase.mdf) connection removed And Connection to Azure database established and Setup for this software is created.

--- Clinics Management System/Clinics Management System/Driver.cs
+++ Clinics Management System/Clinics Management System/Driver.cs
@@ -6,12 +6,15 @@ namespace Clinics_Management_System
 {
     static class Driver
     {
-        static SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='F:\My Git repositories\Clinics-Project\Clinics Management System\Clinics Management System\ClinicsDatabase.mdf';Integrated Security=True;Connect Timeout = 30");
-
+        //Local Database connection
+        //static SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='F:\My Git repositories\Clinics-Project\Clinics Management System\Clinics Management System\ClinicsDatabase.mdf';Integrated Security=True;Connect Timeout = 30");
+        
+        //Azure database connection
+        static readonly SqlConnection conn = new SqlConnection("Data Source=zee-server.database.windows.net;Initial Catalog=\"Clinics Database\";User ID=zeeadmin;Password=zee_admin123;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        /// 
         [STAThread]
         static void Main()
         {
